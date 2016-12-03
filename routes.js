479b279da55b79d036e6eed3ad0d4ec548d47096
koop4 <dilillo.roberto@gmail.com>
2016-12-03 15:34:50 +0100

    front upload added to routes

--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ var isRegistered = require('./middleware/isRegistered'),
     isApproved = require('./middleware/isApproved');
 
 router.get('/reports', isRegistered, isApproved, require('./routes/reports.js'));
+router.post('/document/upload/front', require('./routes/frontUpload.js'));
 
 module.exports = router;
