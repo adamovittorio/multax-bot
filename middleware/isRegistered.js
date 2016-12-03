822f04fa26fdf335ad26fae8cd305189487106b3
koop4 <dilillo.roberto@gmail.com>
2016-12-03 15:02:04 +0100

    isRegistered middleware now has a return block for unregistered user

--- middleware/isRegistered.js
+++ middleware/isRegistered.js
@@ -4,7 +4,33 @@ module.exports = isRegistered
 
 function isRegistered(req, res, next) {
     console.log("Registered");
-    if(true){
+    if(!true){
         return next();
-    }
+    } 
+    else{
+        res.json(requestIdBlock());
+    }  
+}
+
+function requestIdBlock(){
+    var text =  "Per poter effettuare segnalazioni, è necessario verificarsi." + 
+                "Carica le foto front e retro del  tuo documento di identità in questa chat";
+    var btn1 = {
+        "type": "show_block",
+        "block_name": "Welcome Back",
+        "title": "Ho cambiato idea."
+    };
+    var block = {
+        "messages": [{
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": text,
+                    "buttons": [ btn1 ]
+                }
+            }
+        }]
+    };
+    return block;
 }
