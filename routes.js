9c309f24362f734ae074075d0876dbc29ef2bf89
koop4 <dilillo.roberto@gmail.com>
2016-12-03 14:16:52 +0100

    info.js removed. it's now managed by chatfuel

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/info', require('./routes/info.js'));
 router.get('/reports', require('./routes/reports.js'));
 
 module.exports = router;
