75993e2c24e0266b34c78555531df07c7fb37527
koop4 <dilillo.roberto@gmail.com>
2016-12-03 09:45:41 +0100

    comments added to info.js route

--- routes/info.js
+++ routes/info.js
@@ -1,7 +1,10 @@
+/** this routes explain the user how it works and then provides 2 choices:
+ * - I want to know more: sends to multax landing page
+ * - I want to report a law Engrangment: triggers registration-check */
 module.exports = function (req, res) {
     var info = {
         "messages": [
-            { "text": "Lasciami spiegare...", }
+            { "text": "Lasciami spiegare..." }
         ]
     }
     res.json(info);
