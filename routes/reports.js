5e4e4a81d99e365e398e6226af3fee2f6100475c
koop4 <dilillo.roberto@gmail.com>
2016-12-03 10:17:32 +0100

    reports routes added

---
+++ routes/reports.js
@@ -0,0 +1,22 @@
+/** this routes give the user all the reports option: */
+module.exports = function (req, res) {
+    var title = "Cosa vuoi segnalare?";
+    var btn1 = {
+        "type": "show_block",
+        "block_name": "Divieto di sosta",
+        "title": "Show the block!"
+    };
+    var reports = {
+        "messages": [{
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": title,
+                    "buttons": [ btn1 ]
+                }
+            }
+        }]
+    };
+    res.json(reports);
+}
