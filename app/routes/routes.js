04c84c1f2c499f91d4aeb44c110345572103c71b
Vittorio Adamo <adamo.vittorio@gmail.com>
2016-12-06 16:38:55 +0100

    delete duplicate file

--- app/routes/routes.js
+++
@@ -1,9 +0,0 @@
-var router  = require('express').Router();
-
-//middleware
-var isRegistered = require('./middleware/isRegistered'),
-    isApproved = require('./middleware/isApproved');
-
-router.get('/reports', isRegistered, isApproved, require('./reports'));
-
-module.exports = router;
